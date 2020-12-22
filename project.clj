b999277d2163778baacbaee568419dbd01c1b90b
Daniel Barlow <dan@telent.net>
2020-12-22 21:34:06 +0000

    alphabetize deps

--- project.clj
+++ project.clj
@@ -7,14 +7,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [telent/texticlj "0.1.0-SNAPSHOT"]
-                 [hiccup "1.0.5"]
-                 [org.clojure/data.json "0.2.5"]
+                 [aleph "0.4.0-beta2"]
                  [clj-time "0.9.0"]
-                 [juxt/dirwatch "0.2.2"]
                  [compojure "1.3.1"]
+                 [hiccup "1.0.5"]
+                 [juxt/dirwatch "0.2.2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [aleph "0.4.0-beta2"]]
+                 [org.clojure/data.json "0.2.5"]
+                 [telent/texticlj "0.1.0-SNAPSHOT"]
+                 ]
   :main ^:skip-aot yablog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
