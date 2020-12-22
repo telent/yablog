ef35df900cbb52ca59dee6b8c77992cf6eaf2dd5
Daniel Barlow <dan@telent.net>
2020-12-22 22:45:30 +0000

    upgrade deps

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :url "https://github.com/telent/yablog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [aleph "0.4.0-beta2"]
-                 [clj-time "0.9.0"]
-                 [compojure "1.3.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [aleph "0.4.6"]
+                 [clj-time "0.15.2"]
+                 [compojure "1.6.2"]
                  [hiccup "1.0.5"]
-                 [juxt/dirwatch "0.2.2"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/data.json "0.2.5"]
+                 [juxt/dirwatch "0.2.5"]
+                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/data.json "1.0.0"]
                  [telent/texticlj "0.1.0-SNAPSHOT"]
                  ]
   :main ^:skip-aot yablog.core
